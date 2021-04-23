98796c2be3ea1aa41154452e1d71773f6de85293
TildaDares <mathildaudufo@gmail.com>
2021-04-23 23:09:32 +0100

    minor

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -7,7 +7,13 @@ const caesar = (string, key) => {
     if (string[i].match(/[a-z]/i)) {
       if (code + key > lastLetterCode(code)) {
         let lastLetter = lastLetterCode(code);
+
+        // get the number of shifts left after reaching the last alphabet (z/Z)
+        // if w is 116 then (122 - 116) = 6
+        // then the number of shifts left is the (key - 6)
         let shift = key - (lastLetter - code);
+
+        // start from the character before the first alphabet and add the shifts left
         encryptedString += String.fromCharCode(lastLetter - 26 + shift);
       } else {
         encryptedString += String.fromCharCode(code + key);
